53eb7b1607f433c3f19e12262ef4089a9f1ca53d
charmi shah <charmi.shah@marutitech.com>
2023-05-08 15:01:13 +0530

    session-5 completed

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); //Allows the request to continue to the next middleware in line
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public"))); //so that the user cannot access the files mentioned in the public folder through the routes
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express.js</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
